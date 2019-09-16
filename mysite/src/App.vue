b802753cb695132d1be78f31cf338673110f40bb
Cole Hollant <coleghollant@gmail.com>
2019-09-16 19:45:17 -0400

    change zed and pos

--- mysite/src/App.vue
+++ mysite/src/App.vue
@@ -12,6 +12,8 @@ body, html {
   margin: 0;
   padding: 0;
   background-color: #352538;
+  z-index: -100000;
+  position: relative;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
